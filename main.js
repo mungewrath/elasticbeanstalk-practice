3793015534f966d3f8eb050b07d4d8ae0a8bf4fb
Matthew Unrath <matthew.unrath@gmail.com>
2017-10-07 12:53:51 -0700

    Sample application

---
+++ main.js
@@ -0,0 +1,16 @@
+var http = require("http");
+
+/* Hello, World! program in node.js */
+console.log("Hello, World!")
+
+http.createServer(function (request, response) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    // Send the response body as "Hello World"
+    response.end('Hello World. Go away now.\n');
+}).listen(8081);
+   
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
